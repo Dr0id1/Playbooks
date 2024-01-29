31611f09798bcd9bc540c778a96aabe50178ec94
Dr0id1 <maxpage312@gmail.com>
2024-01-29 09:14:07 -0500

    Added logging

---
+++ CheckUpdate.ps1
@@ -0,0 +1,22 @@
+# Spécifiez les noms des scripts existants et les liens des nouvelles versions
+$nomScriptUpdate = "SoftUpdatePS.ps1"
+$lienNouvelleVersionUpdate = "https://raw.githubusercontent.com/Dr0id1/Playbooks/master/Powershell/SoftUpdatePS.ps1"
+
+$nomScriptPersonnalisation = "SoftPersonnalisation.ps1"
+$lienNouvelleVersionPersonnalisation = "https://raw.githubusercontent.com/Dr0id1/Playbooks/master/Powershell/SoftPersonnalisation.ps1"
+
+# Fonction pour télécharger et mettre à jour un script
+function TelechargerEtMettreAJourScript($nomScript, $lienNouvelleVersion) {
+    if ((Invoke-WebRequest -Uri $lienNouvelleVersion).Content -ne (Get-Content -Path $nomScript)) {
+        Invoke-WebRequest -Uri $lienNouvelleVersion -OutFile $nomScript
+        Write-Host "Nouvelle version de $nomScript téléchargée avec succès."
+    } else {
+        Write-Host "Aucune nouvelle version de $nomScript disponible."
+    }
+}
+
+# Téléchargez et mettez à jour le premier script
+TelechargerEtMettreAJourScript $nomScriptUpdate $lienNouvelleVersionUpdate
+
+# Téléchargez et mettez à jour le deuxième script
+TelechargerEtMettreAJourScript $nomScriptPersonnalisation $lienNouvelleVersionPersonnalisation
--- Powershell/SoftPersonnalisation.ps1
+++ Powershell/SoftPersonnalisation.ps1
@@ -27,7 +27,7 @@ Write-Host "Select programs to install (type 'all' for all programs):"
 Write-Host "1. VLC"
 Write-Host "2. Adobe Reader"
 Write-Host "3. OpenOffice"
-Write-Host "4. TeamViewer"
+Write-Host "4. 7zip"
 
 # Prompt user to enter program numbers separated by commas or 'all'
 $choices = Read-Host "Enter the numbers of the programs you want to install (comma-separated) or type 'all' for all programs"
@@ -45,10 +45,78 @@ foreach ($choice in $choicesArray) {
         1 { choco install vlc -y }
         2 { choco install adobereader -y }
         3 { choco install openoffice -y }
-        4 { choco install teamviewer -y }
+        4 { choco install 7zip -y }
         default { Write-Host "Invalid choice: $choice. Skipping." }
     }
 }
+
+# Copy wallpaper1.bmp to user's pictures folder
+Write-Host "Copie du wallpaper sur le système..."
+Copy-Item "$PWD\wallpaper1.bmp" "$env:USERPROFILE\pictures\wallpaper1.bmp" -Force
+Write-Host "Done." -f Green
+
+# Skip Line
+write-host "`n"
+
+# Set wallpaper path in registry
+Write-Host "Modification du wallpaper dans le registre..."
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name Wallpaper -Value "$env:USERPROFILE\pictures\wallpaper1.bmp"
+Write-Host "Done." -f Green
+
+# Skip Line
+write-host "`n"
+
+# Set wallpaper style in registry
+Write-Host "Modification du style du wallpaper dans le registre..."
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name WallpaperStyle -Value 2
+Write-Host "Done." -f Green
+
+# Skip Line
+write-host "`n"
+
+# Set tile wallpaper option in registry
+Write-Host "Modification de l'option wallpaper dans le registre..."
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name TileWallpaper -Value 0
+Write-Host "Done." -f Green
+
+# Skip Line
+write-host "`n"
+
+# Personalization
+Write-Host "Ajout des informations de PC Suprême dans Info Système..."
+Copy-Item "$PWD\OEMlogo.bmp" "$env:LOCALAPPDATA\Microsoft\OEMlogo.bmp" -Force
+Copy-Item "$PWD\oeminfo_win10.reg" "$env:LOCALAPPDATA\Microsoft\oeminfo_win10.reg" -Force
+Start-Process regedit.exe -ArgumentList "/s $env:LOCALAPPDATA\Microsoft\oeminfo_win10.reg" -Wait
+Remove-Item "$env:LOCALAPPDATA\Microsoft\oeminfo_win10.reg" -Force
+
+# Modify additional registry key
+$regKeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation'
+$regKeyValues = @{
+    'Manufacturer' = 'PC Suprême Inc.'
+    'Logo' = "$env:USERPROFILE\Local Settings\Application Data\Microsoft\OEMlogo.bmp"
+    'SupportPhone' = '514-644-1331 // 1-877-301-4809'
+    'SupportHours' = 'Lundi au mercredi : 9h30-17h30. Jeudi et Vendredi : 9h30-21h'
+}
+
+New-Item -Path $regKeyPath -Force
+foreach ($entry in $regKeyValues.GetEnumerator()) {
+    Set-ItemProperty -Path $regKeyPath -Name $entry.Key -Value $entry.Value
+}
+Write-Host "Done." -f Green
+
+# Skip Line
+write-host "`n"
+
+# Refresh the desktop
+Write-Host "Rafraichissement du bureau..."
+[Microsoft.Win32.SystemEvents]::UserPreferenceChanged.Invoke($null, [Microsoft.Win32.UserPreferenceChangedEventArgs]::Desktop)
+Write-Host "Done." -f Green
+
+# Wait for the desktop to finish refreshing
+Start-Sleep -Seconds 2
+
+# Skip Line
+write-host "`n"
 
 # Done
 Write-Host -NoNewLine "Appuyer sur n'importe quel touche pour quitter...";
