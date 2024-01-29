713d6d47be3ed6b9dcb379c4e91504b4d012dcf3
Dr0id1 <maxpage312@gmail.com>
2024-01-29 11:56:26 -0500

    Made some corrections

--- Powershell/SoftPersonnalisation.ps1
+++ Powershell/SoftPersonnalisation.ps1
@@ -10,7 +10,7 @@ $ProgressPreference = 'SilentlyContinue'
 
 # Check for elevated privileges and restart the script with elevated privileges if not already elevated
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "Please run this script as an administrator."
+    Write-Host "Ce script doit être executer en tant qu'administrateur. Redemarrage en cours ..."
     Start-Sleep -Seconds 3
     Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     Exit
@@ -21,34 +21,6 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 
 # Clear output
 Clear-Host
-
-# Display program choices
-Write-Host "Select programs to install (type 'all' for all programs):"
-Write-Host "1. VLC"
-Write-Host "2. Adobe Reader"
-Write-Host "3. OpenOffice"
-Write-Host "4. 7zip"
-
-# Prompt user to enter program numbers separated by commas or 'all'
-$choices = Read-Host "Enter the numbers of the programs you want to install (comma-separated) or type 'all' for all programs"
-
-# Convert the input into an array of choices
-$choicesArray = $choices -split ',' | ForEach-Object { $_.Trim() }
-
-# Install the chosen programs using Chocolatey
-foreach ($choice in $choicesArray) {
-    switch ($choice) {
-        'all' {
-            choco install vlc adobereader openoffice teamviewer -y
-            break
-        }
-        1 { choco install vlc -y }
-        2 { choco install adobereader -y }
-        3 { choco install openoffice -y }
-        4 { choco install 7zip -y }
-        default { Write-Host "Invalid choice: $choice. Skipping." }
-    }
-}
 
 # Copy wallpaper1.bmp to user's pictures folder
 Write-Host "Copie du wallpaper sur le système..."
@@ -83,7 +55,7 @@ Write-Host "Done." -f Green
 write-host "`n"
 
 # Personalization
-Write-Host "Ajout des informations de PC Suprême dans Info Système..."
+Write-Host "Ajout des informations de PC Suprême dans Info Systeme..."
 Copy-Item "$PWD\OEMlogo.bmp" "$env:LOCALAPPDATA\Microsoft\OEMlogo.bmp" -Force
 Copy-Item "$PWD\oeminfo_win10.reg" "$env:LOCALAPPDATA\Microsoft\oeminfo_win10.reg" -Force
 Start-Process regedit.exe -ArgumentList "/s $env:LOCALAPPDATA\Microsoft\oeminfo_win10.reg" -Wait
@@ -92,7 +64,7 @@ Remove-Item "$env:LOCALAPPDATA\Microsoft\oeminfo_win10.reg" -Force
 # Modify additional registry key
 $regKeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation'
 $regKeyValues = @{
-    'Manufacturer' = 'PC Suprême Inc.'
+    'Manufacturer' = 'PC Supreme Inc.'
     'Logo' = "$env:USERPROFILE\Local Settings\Application Data\Microsoft\OEMlogo.bmp"
     'SupportPhone' = '514-644-1331 // 1-877-301-4809'
     'SupportHours' = 'Lundi au mercredi : 9h30-17h30. Jeudi et Vendredi : 9h30-21h'
@@ -116,6 +88,40 @@ Write-Host "Done." -f Green
 Start-Sleep -Seconds 2
 
 # Skip Line
+write-host "`n"
+
+# Display program choices
+Write-Host "Sélectionnez les programmes à installer (tapez 'all' pour tous les programmes) :"
+Write-Host "1. VLC"
+Write-Host "2. Adobe Reader"
+Write-Host "3. OpenOffice"
+Write-Host "4. 7zip"
+Write-Host "5. Zoom"
+
+# Prompt user to enter program numbers separated by commas or 'all'
+$choices = Read-Host "Entrez les numéros des programmes que vous souhaitez installer (séparés par des virgules) ou tapez 'all' pour tous les programmes."
+
+# Convert the input into an array of choices
+$choicesArray = $choices -split ',' | ForEach-Object { $_.Trim() }
+
+# Install the chosen programs using Chocolatey
+foreach ($choice in $choicesArray) {
+    switch ($choice) {
+        'all' {
+            choco install vlc adobereader openoffice 7zip Zoom -y
+            break
+        }
+        1 { choco install vlc -y }
+        2 { choco install adobereader -y }
+        3 { choco install openoffice -y }
+        4 { choco install 7zip -y }
+        4 { choco install zoom -y }
+        default { Write-Host "Choix invalide: $choice. Skipping." }
+    }
+}
+
+# Skip Line
+write-host "`n"
 write-host "`n"
 
 # Done
